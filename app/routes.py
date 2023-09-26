0d67505602d6e5aea46c55ab90dadb5c226b7d79
olgsfrt <35260629+olgsfrt@users.noreply.github.com>
2023-09-26 11:24:39 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, url_for, send_file
 from app import app
 from app.forms import RepoForm
 from src.data_processing import process_repositories, save_data_to_excel
+from flask import send_from_directory
 import os
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,3 +16,7 @@ def index():
         filepath = os.path.join('data', filename)
         return send_file(filepath, as_attachment=True)
     return render_template('index.html', form=form)
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(os.path.join(app.root_path, 'data'), filename, as_attachment=True)
