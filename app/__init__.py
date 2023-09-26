64055df76bffb2bdc621328a0acdc42d09415de8
olgsfrt <35260629+olgsfrt@users.noreply.github.com>
2023-09-26 11:05:30 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,9 @@ from flask import Flask
 # Erstellen Sie eine Instanz der Flask-Anwendung
 app = Flask(__name__)
 
+# Setzen Sie die geheime Schlüssel
+app.config['SECRET_KEY'] = '123457891111'
+
 # Importieren Sie die Routen/Endpunkte der Anwendung
 from app import routes
+
