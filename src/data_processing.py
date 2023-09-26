cbe5fa10bb71fce9876a682c4242e6a73ba59eed
olgsfrt <35260629+olgsfrt@users.noreply.github.com>
2023-09-26 11:56:49 +0200

    Update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -5,6 +5,7 @@ import os
 
 
 def process_repositories(repo_links, auth_token=None):
+    from app import output_folder  # Importieren Sie output_folder hier
     all_data = {}
     for repo_url in repo_links:
         # Extract owner and repo name from the URL
@@ -26,12 +27,15 @@ def process_repositories(repo_links, auth_token=None):
 
     return all_data
 
+
 def save_data_to_excel(repositories_data):
+    from app import output_folder
     for repo, data in repositories_data.items():
-        output_filename = f'data/contributors_{repo}.xlsx'
+        output_filename = f'contributors_{repo}.xlsx'
+        filepath = os.path.join(output_folder, output_filename)  # Verwenden Sie output_folder für den Dateipfad
         df = pd.DataFrame(data, columns=["Username", "Contributions", "Profile Link", "Followers Link", "Public Repos", "Followers", "Following", "Public Gists", "Repo Topics", "Location"])
 
-        with pd.ExcelWriter(output_filename) as writer:
+        with pd.ExcelWriter(filepath) as writer:  # Verwenden Sie den vollständigen Dateipfad beim Speichern
             df.to_excel(writer, sheet_name=repo)
 
-        print(f"Data for {repo} exported to {output_filename}")
+        print(f"Data for {repo} exported to {filepath}")  # Drucken Sie den vollständigen Dateipfad
