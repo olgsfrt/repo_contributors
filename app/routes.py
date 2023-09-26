9ad75770dd4ebfb3a26ada603b320f2a478857bc
olgsfrt <35260629+olgsfrt@users.noreply.github.com>
2023-09-26 11:00:18 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, send_file
 from app import app
+from app.forms import RepoForm
+from src.data_processing import process_repositories, save_data_to_excel
+import os
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = RepoForm()
+    if form.validate_on_submit():
+        repo_url = form.repo_url.data
+        repositories_data = process_repositories([repo_url])
+        save_data_to_excel(repositories_data)
+        filename = f"contributors_{repo_url.split('/')[-1]}.xlsx"
+        filepath = os.path.join('data', filename)
+        return send_file(filepath, as_attachment=True)
+    return render_template('index.html', form=form)
